9826e490ae02464c61a3ee8232e2a96df520af1a
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2025-06-12 17:13:21 +0100

    spa with react - creating and adding components with react

--- P0_SPA/spa_with_react_no_toolchain.js
+++ P0_SPA/spa_with_react_no_toolchain.js
@@ -2,8 +2,8 @@ import React from "https://esm.sh/react@19/?dev"
 import ReactDOMClient from "https://esm.sh/react-dom@19/client?dev"
 
 window.onload = () => {
-    const rootElement = document.getElementById("root")
-    const root = ReactDOMClient.createRoot(rootElement);
+
+    const root = ReactDOMClient.createRoot(document.getElementById("root"));
 
     const ints = [1, 2, 3];
 
@@ -16,7 +16,7 @@ window.onload = () => {
         React.createElement("li", {key:ints[1]}, ints[1])
     )
     childElements.push(
-        React.createElement("li", {key:ints[1]}, ints[1])
+        React.createElement("li", {key:ints[2]}, ints[2])
     )
 
     root.render(childElements)
