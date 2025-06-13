d8d2415222ffe1ddc32c9aa4a1367158c85e9736
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2025-06-13 10:50:43 +0100

    cjanging component state

--- P01_REACT_INTRO/src/app/page.js
+++ P01_REACT_INTRO/src/app/page.js
@@ -2,9 +2,10 @@
 import {useState} from "react";
 export default function Home() {
 
-    function ListItems({ints}) {
+    function ListItems({ints, items}) {
         return (
             <>
+                <button onClick={addValue}>Add Item</button>
                 {
                     ints.map(id => {
                         return (
@@ -18,9 +19,14 @@ export default function Home() {
 
     const [ints,setInts] = useState([1,2,3])
 
+    function addValue() {
+        const newValue = Math.max(...ints) + 1
+        setInts([...ints, newValue])
+    }
+
     return (
         <ul>
-            <ListItems ints={ints} />
+            <ListItems ints={ints} addValue={addValue}/>
         </ul>
     )
 }
