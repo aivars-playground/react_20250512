f00e1ae4fdc3b68624dca6fb29b4ac1f192bbdb2
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2025-06-16 16:56:33 +0100

    adding override to a component

--- p03_react_fundamentals_vite/README.md
+++ p03_react_fundamentals_vite/README.md
@@ -21,8 +21,16 @@ styling
 load from cdn `href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"`  
 add overrides from `public/globals.css` as `<link rel="stylesheet" href="/globals.css" />`  
 
-note!!!  
+* note!!!  
 public folder is not automatically reloaded in dev mode.. move customized css to sources `/src`
 [App.css](src/App.css)  and `import "./App.css"` in [App.jsx](src/App.jsx)
+
+* note!!!  
+all custom css files are merged together
+
+bootstrap grid system
+---------------------
+https://getbootstrap.com/docs/4.0/layout/grid/
+
 
 
--- p03_react_fundamentals_vite/src/components/Banner.jsx
+++ p03_react_fundamentals_vite/src/components/Banner.jsx
@@ -2,13 +2,15 @@ import logo from "../assets/GloboLogo.png"
 
 const Banner = () => {
     return (
-        <header>
-            <div>
-                <img src={logo} alt="logo" />
+        <header className="row mb-4">
+            <div className="col-5">
+                <img src={logo} className="logo" alt="logo" />
             </div>
-            <div>This is a banner component</div>
+            <div className="col-7">
+                Providing houses all over the world
+            </div>
         </header>
-    )
+    );
 }
 
-export default Banner
+export default Banner;
