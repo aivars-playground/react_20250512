a75387f17ffd7034c8317e08a84109d996916b24
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2025-06-16 15:15:24 +0100

    adding banner with image in public folder

---
+++ p03_react_fundamentals_vite/src/components/Banner.jsx
@@ -0,0 +1,12 @@
+const Banner = () => {
+    return (
+        <header>
+            <div>
+                <img src="./GloboLogo.png" alt="logo" />
+            </div>
+            <div>This is a banner component</div>
+        </header>
+    )
+}
+
+export default Banner
